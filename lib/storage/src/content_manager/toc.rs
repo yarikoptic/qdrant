8a30538276273bc10e931ceae9f617bfb868ecdb
timvisee <tim@visee.me>
2023-06-15 10:27:03 +0200

    Add logic to update vector HNSW in collection

--- lib/storage/src/content_manager/toc.rs
+++ lib/storage/src/content_manager/toc.rs
@@ -741,6 +741,9 @@ impl TableOfContent {
             collection.update_vectors_from_diff(diff).await?;
             recreate_optimizers = true;
         }
+        if let Some(diff) = vectors {
+            collection.update_vectors_from_diff(diff).await?;
+        }
         if let Some(changes) = replica_changes {
             collection.handle_replica_changes(changes).await?;
         }
